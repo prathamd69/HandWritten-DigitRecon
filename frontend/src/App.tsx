4713091dcf4d1effb2389deea421541353ce7c9f
Dhruv Anand <ananddhruv29@gmail.com>
2025-07-24 20:00:35 +0530

    Implement video stream toggle functionality and enhance loading state management

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,8 +6,13 @@ function App() {
 	const videoRef = useRef<HTMLVideoElement>(null);
 
 	const [isLoading, setIsLoading] = useState(true);
+	const [enableVid, setEnableVid] = useState(true);
 
 	useEffect(() => {
+		requestVideoStream();
+	}, []);
+
+	const requestVideoStream = () => {
 		navigator.mediaDevices
 			.getUserMedia({ video: true })
 			.then((stream) => {
@@ -19,28 +24,43 @@ function App() {
 			.finally(() => {
 				setIsLoading(false);
 			});
-	}, []);
+	};
+
+	const toggleVideoStream = () => {
+		if (enableVid) {
+			setVid(undefined);
+			setEnableVid(false);
+		} else {
+			requestVideoStream();
+			setEnableVid(true);
+		}
+	};
 
 	useEffect(() => {
-		if (videoRef.current && vid) {
+		if (videoRef.current && vid && enableVid) {
 			videoRef.current.srcObject = vid;
 		}
-	}, [vid]);
+	}, [vid, enableVid]);
 
 	return (
 		<>
 			<div className="container">
 				<h1>Digit Recognition</h1>
 				<div>
-					<video
-						autoPlay
-						playsInline
-						ref={videoRef}
-						className={`${isLoading ? "skeleton" : "loaded"}`}
-					/>
+					{enableVid && (
+						<video
+							autoPlay
+							playsInline
+							ref={videoRef}
+							className={`${isLoading ? "skeleton" : "loaded"}`}
+						/>
+					)}
 					<img />
 				</div>
 				<button>Capture</button>
+				<button onClick={toggleVideoStream}>
+					{enableVid ? "Disable" : "Enable"} Stream
+				</button>
 				<p>Predicted Number : ~</p>
 			</div>
 		</>
