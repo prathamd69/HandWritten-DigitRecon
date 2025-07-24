88ff8a5871df35c7a7acc79fbda66115edf149e3
Dhruv Anand <ananddhruv29@gmail.com>
2025-07-24 19:19:12 +0530

    Refactor code structure and remove unused assets; update ESLint configuration for improved linting rules.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [vid, setVid] = useState<MediaStream | undefined>(undefined);
+	const videoRef = useRef<HTMLVideoElement>(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+			setVid(stream);
+		});
+	}, []);
+
+	useEffect(() => {
+		if (videoRef.current && vid) {
+			videoRef.current.srcObject = vid;
+		}
+	}, [vid]);
+
+	return (
+		<>
+			<div className="container">
+				<h1>Digit Recognition</h1>
+				<div>
+					<video autoPlay playsInline ref={videoRef} />
+					<img />
+				</div>
+				<button>Capture</button>
+				<p>Predicted Number : ~</p>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
